83a8cd805ebc40d41daead53bd6d1d83354ecd4c
Laura medina <laurasofiamedinamera@gmail.com>
2026-07-13 16:53:14 -0500

    falta

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Product extends Model
 {
@@ -20,6 +21,7 @@ class Product extends Model
         'category',
         'stock',
     ];
+    protected $allowIncluded=['order_details','categories','options'];
 
     // Relacion Uno a Muchos 
     public function order_details()
@@ -38,5 +40,33 @@ class Product extends Model
     {
         return $this->belongsToMany(Option::class, 'product_options');
     }
+
+    
+/////////////////////////////////////////////////////////////////////////////
+    public function scopeIncluded(Builder $query){
+       
+        if(empty($this->allowIncluded)||empty(request('included'))){
+            return $query;
+        }
+        $relations = explode(',', request('included'));//['posts','relation2']
+       
+       $allowIncluded=collect($this->allowIncluded);//colocamos en una colecion lo que tiene $allowIncluded en este caso = ['posts','posts.user']
+    
+        foreach($relations as $key => $relationship){//recorremos el array de relaciones
+            
+            if(!$allowIncluded->contains($relationship)){
+                unset($relations[$key]);
+            }
+        
+        }
+
+     return $query->with($relations);//se ejecuta el query con lo que tiene $relations en ultimas es el valor en la url de included
+     
+    }
+///////////////////////////////////////////////////////////////////////////////////////////
+
+public function scopeFilter(Builder $query){
+    return $query;
+}
 
 }
